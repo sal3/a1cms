cea1b8be2439f2999da2d3279b56534dbeccc8ed
vla7 <vlad7rpk@gmail.com>
2013-10-01 02:19:56 +0300

    little fixes mysql errors when no connect

--- sys/dbconfig.php
+++ sys/dbconfig.php
@@ -2,10 +2,10 @@
 if (!defined('a1cms'))
 	die('Access denied to dbconfig.php!');
 
-$host = 's2.tcphost.net';
-$dbase_user_name = 'a1dev';
-$dbase_user_password = 'u1pwfjyZ';
-$db_name = 'a1cmsdev';
+$host = '';
+$dbase_user_name = '';
+$dbase_user_password = '';
+$db_name = '';
 $table_prefix = 'a1';
 
 ?>
